e4194aaa5c09e77f0211ce258eb5ec73278a3613
avi zemmel <avizemmel7227@gmail.com>
2023-06-18 10:01:28 +0300

    Add project files.

---
+++ aviZemmelExam1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    internal class Program
+    {
+        static int kefel()
+        {
+            int sum = 0;
+            Console.WriteLine("enter a number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter a number");
+            int num2 = int.Parse(Console.ReadLine());
+
+            while (num1 > 0)
+            {
+                sum = sum + num2;
+                num1--;
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(kefel());
+
+        }
+    }
+}
